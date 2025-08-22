22eedb5f0cf755f366a0b40949b6cba7d6050250
Alex Santos <devalexsantos@gmail.com>
2025-08-22 11:26:05 -0300

    chore: add GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Providers } from "./providers";
 import NextTopLoader from 'nextjs-toploader';
 import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,12 +35,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        {/* GTM base */}
+        <Script id="gtm-base" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MMQVRGG3');`}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${playfair.variable} antialiased bg-zinc-950 text-zinc-100`}
       >
+        {/* GTM noscript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MMQVRGG3"
+            height="0" width="0" style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        <NextTopLoader />
         <Analytics />
         <SpeedInsights />
-        <NextTopLoader />
         <Providers>
           {children}
         </Providers>
